069734767329c79f761f239b07d263bd26987ce0
bolvan <k@vodka.home.kg>
2016-02-22 19:11:59 +0300

    get_antizapret parse improvement

--- ipset/get_antizapret.sh
+++ ipset/get_antizapret.sh
@@ -11,7 +11,7 @@ ZIPLISTTMP=/tmp/zapret-ip.txt
 
 $EXEDIR/get_user.sh
 
-curl --fail --max-time 60 --max-filesize 4194304 "$ZURL" | sed -nre "s/^.*\"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\".*$/\1/p" >$ZIPLISTTMP &&
+curl --fail --max-time 60 --max-filesize 4194304 "$ZURL" | sed -nre "s/\"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\"/\1\n/gp" | sed -nre "s/^[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$/\1/p" >$ZIPLISTTMP &&
 {
  dlsize=$(wc -c "$ZIPLISTTMP" | cut -f 1 -d ' ')
  if test $dlsize -lt 20480; then
