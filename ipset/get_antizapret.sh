8283ea04bdee323a62caeaf2d9d181f7b620bcda
bolvan <k@vodka.home.kg>
2016-11-07 18:01:23 +0300

    get_anizapret update

--- ipset/get_antizapret.sh
+++ ipset/get_antizapret.sh
@@ -6,12 +6,12 @@ EXEDIR=$(dirname $SCRIPT)
 
 . "$EXEDIR/def.sh"
 
-ZURL=http://antizapret.prostovpn.org/proxy.pac
+ZURL=https://github.com/zapret-info/z-i/raw/master/dump.csv
 ZIPLISTTMP=/tmp/zapret-ip.txt
 
 getuser
 
-curl --fail --max-time 60 --max-filesize 4194304 "$ZURL" | sed -nre "s/\"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\"/\1\n/gp" | sed -nre "s/^[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$/\1/p" >$ZIPLISTTMP &&
+curl --fail --max-time 60 --max-filesize 33554432 -k -L "$ZURL" | sed -nre "s/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/\1\n/gp" | sed -nre "s/^[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$/\1/p" >$ZIPLISTTMP &&
 {
  dlsize=$(wc -c "$ZIPLISTTMP" | cut -f 1 -d ' ')
  if test $dlsize -lt 20480; then
