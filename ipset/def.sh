667afc814eb77d6983567dc7ba39e4c7e7f6b2c2
bolvan <k@vodka.home.kg>
2018-07-21 09:26:03 +0300

    more chmod fixes

--- ipset/def.sh
+++ ipset/def.sh
@@ -4,11 +4,13 @@ ZIPLIST=$EXEDIR/zapret-ip.txt
 ZIPLIST_EXCLUDE=$EXEDIR/zapret-ip-exclude.txt
 ZIPLIST_USER=$EXEDIR/zapret-ip-user.txt
 ZUSERLIST=$EXEDIR/zapret-hosts-user.txt
+ZHOSTLIST=$EXEDIR/zapret-hosts.txt
 
 ZIPSET_IPBAN=ipban
 ZIPLIST_IPBAN=$EXEDIR/zapret-ip-ipban.txt
 ZIPLIST_USER_IPBAN=$EXEDIR/zapret-ip-user-ipban.txt
 ZUSERLIST_IPBAN=$EXEDIR/zapret-hosts-user-ipban.txt
+
 
 getuser()
 {
