27e02b07131fb2803087807eab89b9030bee1512
bolvan <k@vodka.home.kg>
2019-05-02 10:22:47 +0300

    use ln instead of cp to save disk

--- install_bin.sh
+++ install_bin.sh
@@ -2,26 +2,32 @@
 
 SCRIPT=$(readlink -f $0)
 EXEDIR=$(dirname $SCRIPT)
-BINDIR=$EXEDIR/binaries
+BINS=binaries
+BINDIR=$EXEDIR/$BINS
 
 check_dir()
 {
- echo 0.0.0.0 | $BINDIR/$1/ip2net 1>/dev/null 2>/dev/null
+ echo 0.0.0.0 | "$BINDIR/$1/ip2net" 1>/dev/null 2>/dev/null
 }
+
+# link or copy executables. uncomment either ln or cp, comment other
 ccp()
 {
- cp -f $1 $2 && echo "$1" =\> "$2"
+ local F=$(basename $1)
+ [ -f "$2/$F" ] && rm -f "$2/$F"
+ ln -s "../$BINS/$1" "$EXEDIR/$2" && echo linking : "../$BINS/$1" =\> "$EXEDIR/$2"
+ #cp -f "$BINDIR/$1" "$EXEDIR/$2" && echo copying : "$BINDIR/$1" =\> "$EXEDIR/$2"
 }
 
 for arch in aarch64 armhf mips32r1-lsb mips32r1-msb x86_64 x86
 do
  if check_dir $arch; then
   echo $arch is OK
-  echo copying binaries ...
-  ccp $BINDIR/$arch/ip2net $EXEDIR/ip2net
-  ccp $BINDIR/$arch/mdig $EXEDIR/mdig
-  ccp $BINDIR/$arch/nfqws $EXEDIR/nfq
-  ccp $BINDIR/$arch/tpws $EXEDIR/tpws
+  echo installing binaries ...
+  ccp $arch/ip2net ip2net
+  ccp $arch/mdig mdig
+  ccp $arch/nfqws nfq
+  ccp $arch/tpws tpws
   break
  else
   echo $arch is NOT OK
