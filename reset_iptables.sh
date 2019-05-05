500ee21f6270c3f843813b827f6c1f60c83971bb
Alexander <amakcross@gmail.com>
2019-05-05 02:29:11 +0000

    Created a script to reset iptables rules
    Создал скрипт для сброса правил iptables

---
+++ reset_iptables.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Flushing iptables rules..."
+sleep 1
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
