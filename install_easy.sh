95979d0fa30011cfc4bb6909a37fa052af968ed3
bolvan <k@vodka.home.kg>
2019-05-07 19:34:11 +0300

    install_easy : fix * in rm old lists

--- install_easy.sh
+++ install_easy.sh
@@ -349,8 +349,8 @@ download_list()
 		echo \* downloading blocked ip/host list ...
 
 		# can be txt or txt.gz
-		rm -f "$EXEDIR/ipset/zapret-ip.txt*" "$EXEDIR/ipset/zapret-ip-user.txt*" \
-			"$EXEDIR/ipset/zapret-ip-ipban.txt*" "$EXEDIR/ipset/zapret-ip-user-ipban.txt*"
+		rm -f "$EXEDIR/ipset/zapret-ip.txt"* "$EXEDIR/ipset/zapret-ip-user.txt"* \
+			"$EXEDIR/ipset/zapret-ip-ipban.txt"* "$EXEDIR/ipset/zapret-ip-user-ipban.txt"*
 		"$GET_LIST" || {
 			echo could not download ip list
 			exitp 25
