99b3b3f4338aadb7418176153ae996d626545444
bol-van <k@vodka.home.kg>
2021-11-16 11:58:57 +0300

    ipset: gawk C locale

--- ipset/get_reestr_ip.sh
+++ ipset/get_reestr_ip.sh
@@ -13,7 +13,7 @@ ZURL_REESTR=https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv
 awkgrep()
 { 
  # $1 - pattern
- nice -n 5 $AWK "{while ( match(\$0,/$1[ |;]/) ) { print substr(\$0,RSTART,RLENGTH-1); \$0=substr(\$0,RSTART+RLENGTH) } }"
+ LANG=C nice -n 5 $AWK "{while ( match(\$0,/$1[ |;]/) ) { print substr(\$0,RSTART,RLENGTH-1); \$0=substr(\$0,RSTART+RLENGTH) } }"
 }
 
 dig_reestr()
