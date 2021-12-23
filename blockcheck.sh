2f5c6b7e82eaf889ba9e3b8e06511a8d6de57cf7
bol-van <none@none.none>
2021-12-23 21:03:52 +0300

    more compatible require_root

--- blockcheck.sh
+++ blockcheck.sh
@@ -87,11 +87,13 @@ ask_yes_no_var()
 
 require_root()
 {
+	local exe
 	echo \* checking privileges
 	[ $(id -u) -ne "0" ] && {
 		echo root is required
-		exists sudo && exec sudo "$0"
-		exists su && exec su -c "$0"
+		exe="$EXEDIR/$(basename "$0")"
+		exists sudo && exec sudo "$exe"
+		exists su && exec su root -c "$exe"
 		echo su or sudo not found
 		exitp 2
 	}
--- install_easy.sh
+++ install_easy.sh
@@ -55,11 +55,13 @@ exitp()
 
 require_root()
 {
+	local exe
 	echo \* checking privileges
 	[ $(id -u) -ne "0" ] && {
 		echo root is required
-		exists sudo && exec sudo "$0"
-		exists su && exec su -c "$0"
+		exe="$EXEDIR/$(basename "$0")"
+		exists sudo && exec sudo "$exe"
+		exists su && exec su root -c "$exe"
 		echo su or sudo not found
 		exitp 2
 	}
