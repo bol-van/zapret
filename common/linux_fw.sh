8c1d1cf37a81b5fd70298205c345d7c339e1a567
bol-van <none@none.none>
2023-10-29 13:04:04 +0300

    tcp_be_liberal info

--- common/linux_fw.sh
+++ common/linux_fw.sh
@@ -20,10 +20,10 @@ zapret_do_firewall()
 
 	# russian DPI sends RST,ACK with wrong ACK.
 	# this is sometimes treated by conntrack as invalid and connbytes fw rules do not pass RST packet to nfqws.
-	# swith on liberal mode on zapret firewall start and switch off on zapret firewall stop
+	# switch on liberal mode on zapret firewall start and switch off on zapret firewall stop
 	# this is only required for processing incoming bad RSTs. incoming rules are only applied in autohostlist mode
 	# calling this after firewall because conntrack module can be not loaded before applying conntrack firewall rules
-	[ "$MODE_FILTER" = "autohostlist" -a "$MODE" != tpws-socks ] && set_conntrack_liberal_mode $1
+	[ "$MODE_FILTER" = "autohostlist" -a "$MODE" != tpws -a "$MODE" != tpws-socks ] && set_conntrack_liberal_mode $1
 	
 	[ "$1" = 1 -a -n "$INIT_FW_POST_UP_HOOK" ] && $INIT_FW_POST_UP_HOOK
 	[ "$1" = 0 -a -n "$INIT_FW_POST_DOWN_HOOK" ] && $INIT_FW_POST_DOWN_HOOK
