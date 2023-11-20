cbb268ddc8d25b81e8d67af4fdeebe396a0a49b8
bol-van <none@none.none>
2023-11-20 23:14:43 +0300

    list.nethub.fi was closed. dirty hack enable

--- ipset/get_reestr_preresolved.sh
+++ ipset/get_reestr_preresolved.sh
@@ -7,6 +7,11 @@ IPSET_DIR="$(cd "$IPSET_DIR"; pwd)"
 
 
 TMPLIST="$TMPDIR/list_nethub.txt"
+
+# free domain was discontinued
+HOST=list.nethub.fi
+IP=69.197.166.36
+RESOLVE="--resolve $HOST:80:$IP"
 URL4="http://list.nethub.fi/reestr_resolved4.txt"
 URL6="http://list.nethub.fi/reestr_resolved6.txt"
 
@@ -15,7 +20,7 @@ dl()
 {
   # $1 - url
   # $2 - file
-  curl -H "Accept-Encoding: gzip" -k --fail --max-time 180 --connect-timeout 10 --retry 4 --max-filesize 33554432 "$1" | gunzip - >"$TMPLIST" ||
+  curl -H "Accept-Encoding: gzip" $RESOLVE -k --fail --max-time 180 --connect-timeout 10 --retry 4 --max-filesize 33554432 "$1" | gunzip - >"$TMPLIST" ||
   {
    echo list download failed : $1
    exit 2
--- ipset/get_reestr_resolvable_domains.sh
+++ ipset/get_reestr_resolvable_domains.sh
@@ -6,13 +6,18 @@ IPSET_DIR="$(cd "$IPSET_DIR"; pwd)"
 . "$IPSET_DIR/def.sh"
 
 TMPLIST="$TMPDIR/list_nethub.txt"
+
+# free domain was discontinued
+HOST=list.nethub.fi
+IP=69.197.166.36
+RESOLVE="--resolve $HOST:80:$IP"
 URL="http://list.nethub.fi/reestr_hostname_resolvable.txt"
 
 dl()
 {
   # $1 - url
   # $2 - file
-  curl -H "Accept-Encoding: gzip" -k --fail --max-time 180 --connect-timeout 10 --retry 4 --max-filesize 33554432 "$1" | gunzip - >"$TMPLIST" ||
+  curl -H "Accept-Encoding: gzip" $RESOLVE -k --fail --max-time 180 --connect-timeout 10 --retry 4 --max-filesize 33554432 "$1" | gunzip - >"$TMPLIST" ||
   {
    echo list download failed : $1
    exit 2
