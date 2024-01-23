85219cfc38ea36357b5a1c9f91f380e3a5ae8126
bol-van <none@none.none>
2024-01-23 13:27:42 +0300

    install_prereq: fix macos

--- install_easy.sh
+++ install_easy.sh
@@ -209,31 +209,6 @@ select_getlist()
 	GETLIST=""
 	write_config_var GETLIST
 }
-select_ipv6()
-{
-	local T=N
-
-	[ "$DISABLE_IPV6" != '1' ] && T=Y
-	local old6=$DISABLE_IPV6
-	echo
-	if ask_yes_no $T "enable ipv6 support"; then
-		DISABLE_IPV6=0
-	else
-		DISABLE_IPV6=1
-	fi
-	[ "$old6" != "$DISABLE_IPV6" ] && write_config_var DISABLE_IPV6
-}
-select_fwtype()
-{
-	echo
-	[ $(get_ram_mb) -le 400 ] && {
-		echo WARNING ! you are running a low RAM system
-		echo WARNING ! nft requires lots of RAM to load huge ip sets, much more than ipsets require
-		echo WARNING ! if you need large lists it may be necessary to fall back to iptables+ipset firewall
-	}
-	echo select firewall type :
-	ask_list FWTYPE "iptables nftables" "$FWTYPE" && write_config_var FWTYPE
-}
 
 ask_config()
 {
--- install_prereq.sh
+++ install_prereq.sh
@@ -15,38 +15,6 @@ ZAPRET_BASE="$EXEDIR"
 . "$ZAPRET_BASE/common/installer.sh"
 . "$ZAPRET_BASE/common/ipt.sh"
 
-select_ipv6()
-{
-	local T=N
-
-	[ "$DISABLE_IPV6" != '1' ] && T=Y
-	local old6=$DISABLE_IPV6
-	echo
-	if ask_yes_no $T "enable ipv6 support"; then
-		DISABLE_IPV6=0
-	else
-		DISABLE_IPV6=1
-	fi
-	[ "$old6" != "$DISABLE_IPV6" ] && write_config_var DISABLE_IPV6
-}
-select_fwtype()
-{
-	echo
-	[ $(get_ram_mb) -le 400 ] && {
-		echo WARNING ! you are running a low RAM system
-		echo WARNING ! nft requires lots of RAM to load huge ip sets, much more than ipsets require
-		echo WARNING ! if you need large lists it may be necessary to fall back to iptables+ipset firewall
-	}
-	echo select firewall type :
-	ask_list FWTYPE "iptables nftables" "$FWTYPE" && write_config_var FWTYPE
-}
-
-ask_config()
-{
-	[ "$SYSTEM" = openwrt ] && select_ipv6
-	select_fwtype
-}
-
 
 # build binaries, do not use precompiled
 [ "$1" = "make" ] && FORCE_BUILD=1
@@ -55,13 +23,17 @@ umask 0022
 fsleep_setup
 fix_sbin_path
 check_system
+[ $UNAME = "Linux" ] || {
+	echo no prerequisites required for $SYSTEM
+}
 require_root
-ask_config
 
 case $UNAME in
 	Linux)
+		select_fwtype
 		case $SYSTEM in
 			openwrt)
+				select_ipv6
 				check_prerequisites_openwrt
 				;;
 			*)
