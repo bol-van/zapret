c9a8fcb3db143309a9b57255097cd2f70f4940d1
bol-van <none@none.none>
2024-04-27 17:31:05 +0300

    winws: russia preset

---
+++ binaries/win64/zapret-winws/preset_russia.cmd
@@ -0,0 +1,2 @@
+start "zapret: http,https" "%~dp0winws.exe" --wf-tcp=80,443 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig
+start "zapret: quic" "%~dp0winws.exe" --wf-udp=443 --dpi-desync=fake --dpi-desync-repeats=10
