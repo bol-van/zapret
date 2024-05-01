e8241a4fa3f64ea22990005c73662aa3050ea72c
bol-van <none@none.none>
2024-05-01 10:11:13 +0300

    preset_russia.cmd : add 1 sec delay before 2 instances

--- binaries/win64/zapret-winws/preset_russia_autohostlist.cmd
+++ binaries/win64/zapret-winws/preset_russia_autohostlist.cmd
@@ -1,2 +1,3 @@
 start "zapret: http,https,autohostlist" "%~dp0winws.exe" --wf-tcp=80,443 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%~dp0autohostlist.txt"
+timeout /t 1
 start "zapret: quic,autohostlist" "%~dp0winws.exe" --wf-udp=443 --dpi-desync=fake --dpi-desync-repeats=10 --hostlist-auto="%~dp0autohostlist.txt"
