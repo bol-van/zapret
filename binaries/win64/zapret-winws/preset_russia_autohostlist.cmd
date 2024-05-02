1369734ebc579c19c942bccb0389587fd0114113
bol-van <none@none.none>
2024-05-02 16:03:35 +0300

    preset cmd: start minimized

--- binaries/win64/zapret-winws/preset_russia_autohostlist.cmd
+++ binaries/win64/zapret-winws/preset_russia_autohostlist.cmd
@@ -1,2 +1,2 @@
-start "zapret: http,https,autohostlist" "%~dp0winws.exe" --wf-tcp=80,443 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%~dp0autohostlist.txt"
-start "zapret: quic,autohostlist" "%~dp0winws.exe" --wf-udp=443 --dpi-desync=fake --dpi-desync-repeats=10 --hostlist-auto="%~dp0autohostlist.txt"
+start "zapret: http,https,autohostlist" "%~dp0winws.exe" /min --wf-tcp=80,443 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%~dp0autohostlist.txt"
+start "zapret: quic,autohostlist" "%~dp0winws.exe" /min --wf-udp=443 --dpi-desync=fake --dpi-desync-repeats=10 --hostlist-auto="%~dp0autohostlist.txt"
