68c80d4006a15d3b2e233622be8c03c33b5dd796
bol-van <none@none.none>
2024-10-26 10:53:11 +0300

    service_create.cmd, task_create.cmd : comment service creation to avoid accidental installation

--- binaries/win64/zapret-winws/service_create.cmd
+++ binaries/win64/zapret-winws/service_create.cmd
@@ -1,5 +1,5 @@
-set ARGS=--wf-l3=ipv4,ipv6 --wf-tcp=80,443 --dpi-desync=fake,split --dpi-desync-ttl=7 --dpi-desync-fooling=md5sig
-call :srvinst winws1
+rem set ARGS=--wf-l3=ipv4,ipv6 --wf-tcp=80,443 --dpi-desync=fake,split --dpi-desync-ttl=7 --dpi-desync-fooling=md5sig
+rem call :srvinst winws1
 rem set ARGS=--wf-l3=ipv4,ipv6 --wf-udp=443 --dpi-desync=fake 
 rem call :srvinst winws2
 goto :eof
